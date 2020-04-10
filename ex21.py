4f00251958fe95279838585c91c6cb105a9d52c9
LHerouin <56364167+LHerouin@users.noreply.github.com>
2020-04-10 12:40:22 +0200

    Correction prof

--- ex21.py
+++ ex21.py
@@ -1,12 +1,12 @@
 #nombre de saisie
 print('Saisir un nombre x')
 x=int(input())
+#ouverture de data.txt et écriture sur le fichier de chaine
+f = open("data.txt", "a")
 #Saisir x chaine
 for i in range(0,x):
 	print('Saisir chaine')
 	chaine = input()
-	#ouverture de data.txt et écriture sur le fichier de chaine
-	f = open("data.txt", "a")
 	f.write(chaine+'\n')
-	#fermeture du fichier
-	f.close()
+#fermeture du fichier
+f.close()
